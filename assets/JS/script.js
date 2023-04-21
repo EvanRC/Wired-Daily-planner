5035d036b6514388319b9f121cebfbedef52e56e
Evan Carlson <evanrc1@gmail.com>
2023-04-21 10:55:15 -0700

    added colors for past, present, and future

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -2,34 +2,57 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-$(document).ready(function() {
+$(document).ready(function () {
   var html = ""
-  for(let i= 9;i<18;i++ ){
-    if(i < 12){
-      var timeBlock = i+"AM"
-    }else if(i === 12){
-      var timeBlock = i+"PM"
-    }else{
- var timeBlock = (i-12)+"PM" // 13-12, 14-12, 15 -12,
-    }
-    var saveList = localStorage.getItem("hour-"+i) || ""
-html += ` <div id="hour-${i}" class="row time-block">
-<div class="col-2 col-md-1 hour text-center py-3">${timeBlock}</div>
-<textarea class="col-8 col-md-10 description" rows="3 val=${saveList}"> ${saveList}</textarea>
-<button class="btn saveBtn col-2 col-md-1" aria-label="save">
-  <i class="fas fa-save" aria-hidden="true"></i>
-</button>
-</div>
-`
+  var currentHour = dayjs().hour()
+  console.log(currentHour)
+  setInterval(function(){
+    $("#currentDay").text(dayjs().format('HH:mm:ss YYYY-MM-DD') )
+  },1000)
+  for (let i = 9; i < 18; i++) {
+            if (i < 12) {
+              var timeBlock = i + "AM"
+            } else if (i === 12) {
+              var timeBlock = i + "PM"
+            } else {
+              var timeBlock = (i - 12) + "PM" // 13-12, 14-12, 15 -12,
+            }
+
+            var saveList = localStorage.getItem("hour-" + i) || ""
+
+            if (i < currentHour) {
+              var planColor = "past"
+            }
+            else if (i === currentHour) {
+              var planColor = "present"
+            }
+            else {
+              var planColor = "future"
+            }
+           // console.log(i,timeBlock,saveList,planColor)
+            html += `
+            
+            <div id="hour-${i}" class="row time-block ${planColor}">
+            <div class="col-2 col-md-1 hour text-center py-3">${timeBlock}</div>
+            <textarea class="col-8 col-md-10 description" rows="3">${saveList} </textarea>
+            <button class="btn saveBtn col-2 col-md-1" aria-label="save">
+              <i class="fas fa-save" aria-hidden="true"></i>
+            </button>
+          </div>
+    
+            
+            `
+      
   }
+  //console.log(html)//
 
-$(".container-fluid").html(html)
+  $(".container-fluid").html(html)
 
- $(".saveBtn").on('click', function(event) {
-  var userdayplan = $(this).siblings("textarea").val()
-  var timeBlock = $(this).parent().attr("id")
-    console.log('Button clicked',userdayplan,timeBlock);
-    localStorage.setItem(timeBlock,userdayplan)
+  $(".saveBtn").on('click', function (event) {
+    var userdayplan = $(this).siblings("textarea").val()
+    var timeBlock = $(this).parent().attr("id")
+    console.log('Button clicked', userdayplan, timeBlock);
+    localStorage.setItem(timeBlock, userdayplan)
 
   });
   // TODO: Add a listener for click events on the save button. This code should
